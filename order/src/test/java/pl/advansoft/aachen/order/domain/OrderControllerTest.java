8248f8f34daf8c1bcc93e7ec247c77e8c333982f
Piotr Borelowski <advansoft-pb@github.com>
2026-09-03 16:08:57 +0200

    the 15th

---
+++ order/src/test/java/pl/advansoft/aachen/order/domain/OrderControllerTest.java
@@ -0,0 +1,48 @@
+package pl.advansoft.aachen.order.domain;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import pl.advansoft.aachen.order.AbstractIT;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+class OrderControllerTest extends AbstractIT {
+
+    @Nested
+    class CreateOrderTests {
+        @Test
+        void shouldCreateOrderSuccessfully() {
+            String payload = """
+                    {
+                      "customer": {
+                        "name": "Stefan",
+                        "phone": "1234567890"
+                      },
+                      "deliveryAddress": {
+                        "zipCode": "12345",
+                        "country": "Polska"
+                      },
+                      "items": [
+                        {
+                          "name": "hej",
+                          "code": "P23",
+                          "price": 0.40,
+                          "quantity": 4
+                        }
+                      ]
+                    }
+                    """;
+            given()
+                    .contentType(ContentType.JSON)
+                    .body(payload)
+                    .when()
+                    .post("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .body("orderNumber", notNullValue());
+        }
+    }
+}

6cad0949ab59b5ae35856a61d53140ae43f0b365
Piotr Borelowski <advansoft-pb@github.com>
2026-09-07 15:47:12 +0200

    21

--- order/src/main/java/pl/advansoft/aachen/order/client/catalog/ProductServiceClient.java
+++ order/src/main/java/pl/advansoft/aachen/order/client/catalog/ProductServiceClient.java
@@ -20,11 +20,16 @@ public class ProductServiceClient {
 
     public Optional<Product> getProductByCode(String code) {
         LOGGER.info("Fetching product for code: {}", code);
-        Product product = restClient
-                .get()
-                .uri("/api/products/{code}", code)
-                .retrieve()
-                .body(Product.class);
-        return Optional.ofNullable(product);
+        try {
+            Product product = restClient
+                    .get()
+                    .uri("/api/products/{code}", code)
+                    .retrieve()
+                    .body(Product.class);
+            return Optional.ofNullable(product);
+        } catch (Exception ex) {
+            LOGGER.error("Error fetching product for code: {}", code, ex);
+            return Optional.empty();
+        }
     }
 }

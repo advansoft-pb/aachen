1f56855924632be1e44a2bc3e4da9302571a616d
Piotr Borelowski <advansoft-pb@github.com>
2026-09-09 15:07:51 +0200

    23-b

--- order/src/main/java/pl/advansoft/aachen/order/domain/OrderEventMapper.java
+++ order/src/main/java/pl/advansoft/aachen/order/domain/OrderEventMapper.java
@@ -1,7 +1,6 @@
 package pl.advansoft.aachen.order.domain;
 
-import pl.advansoft.aachen.order.domain.models.OrderCreatedEvent;
-import pl.advansoft.aachen.order.domain.models.OrderItem;
+import pl.advansoft.aachen.order.domain.models.*;
 
 import java.time.LocalDateTime;
 import java.util.Set;
@@ -17,6 +16,41 @@ class OrderEventMapper {
                 getOrderItems(order),
                 order.getCustomer(),
                 order.getDeliveryAddress(),
+                LocalDateTime.now()
+        );
+    }
+
+    static OrderDeliveredEvent buildOrderDeliveredEvent(OrderEntity order) {
+        return new OrderDeliveredEvent(
+                UUID.randomUUID().toString(),
+                order.getOrderNumber(),
+                getOrderItems(order),
+                order.getCustomer(),
+                order.getDeliveryAddress(),
+                LocalDateTime.now()
+        );
+    }
+
+    static OrderCancelledEvent buildOrderCancelledEvent(OrderEntity order, String reason) {
+        return new OrderCancelledEvent(
+                UUID.randomUUID().toString(),
+                order.getOrderNumber(),
+                getOrderItems(order),
+                order.getCustomer(),
+                order.getDeliveryAddress(),
+                reason,
+                LocalDateTime.now()
+        );
+    }
+
+    static OrderErrorEvent buildOrderErrorEvent(OrderEntity order, String reason) {
+        return new OrderErrorEvent(
+                UUID.randomUUID().toString(),
+                order.getOrderNumber(),
+                getOrderItems(order),
+                order.getCustomer(),
+                order.getDeliveryAddress(),
+                reason,
                 LocalDateTime.now()
         );
     }

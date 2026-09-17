1fc19226b31c742d0961f3a42c528f2dc01f31e1
Piotr Borelowski <advansoft-pb@github.com>
2026-09-17 16:30:28 +0200

    43

--- order/src/test/java/pl/advansoft/aachen/order/domain/OrderControllerUnitTest.java
+++ order/src/test/java/pl/advansoft/aachen/order/domain/OrderControllerUnitTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -43,8 +42,6 @@ class OrderControllerUnitTest {
     private OrderService orderService;
     @MockitoBean
     private SecurityService securityService;
-    @MockitoBean
-    private HttpSecurity httpSecurity;
     @Autowired
     private MockMvc mockMvc;
     @Autowired
@@ -76,11 +73,13 @@ class OrderControllerUnitTest {
     }
 
     @Test
+    @WithMockUser
     void shouldReturnProperRequestWhenOrderPayloadIsValid() throws Exception {
         given(orderService.createOrder(eq(USER), any(CreateOrderRequest.class))).willReturn(new CreateOrderResponse(NUMBER));
 
         mockMvc
                 .perform(post("/api/orders")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(TestDataFactory.createOrderRequest())))
                 .andExpect(status().isCreated())
